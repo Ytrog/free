8a43d56b87b1bac65f860359b8e1a9c545d36798
Ytrog <hisbrucker@gmail.com>
2021-03-10 14:11:37 +0100

    Print sizes in rounded KB MB or GB formats

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
+use std::path::Path;
 use sysinfo::*;
+
+/// format the size to bytes, KB, MB or GB
+fn format_size(size: u64) -> String {
+    if size > 1000_000_000 { // GB
+        format!("{} GB", size / 1000_000_000)
+    } else if size > 1000_000 { // MB
+        format!("{} MB", size / 1000_000)    
+    } else if size > 1000 { // KB
+        format!("{} KB", size / 1000)
+    } else {
+        size.to_string()
+    }
+}
+
+/// print the disk info to output
+fn print_disk(mount: &Path, total: u64, free: u64) -> () {
+    println!("{} total:{} free: {}", mount.display(), format_size(total), format_size(free));
+}
 
 fn main() {
     let s = System::new_all();
     let disks = s.get_disks();
     for d in disks {
-        println!("{:?}", d);
+        let mount = d.get_mount_point();
+        let total = d.get_total_space();
+        let free = d.get_available_space();
+
+        print_disk(mount, total, free);
     }
 }
