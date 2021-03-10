2ac0e6700d833090f4eeb6a7fa5aa94ec2592324
Ytrog <hisbrucker@gmail.com>
2021-03-10 13:11:01 +0100

    Print all disks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use sysinfo::*;
+
 fn main() {
-    println!("Hello, world!");
+    let s = System::new_all();
+    let disks = s.get_disks();
+    for d in disks {
+        println!("{:?}", d);
+    }
 }
